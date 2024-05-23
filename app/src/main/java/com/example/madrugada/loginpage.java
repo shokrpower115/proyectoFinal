764bec710d407bc4dc9cd21c2c25cc0e2d239f92
Moises Barraza <moisesbarraza115@gmail.com>
2024-05-23 06:30:24 -0700

    proyecto avance

---
+++ app/src/main/java/com/example/madrugada/loginpage.java
@@ -0,0 +1,12 @@
+package com.example.madrugada;
+
+import android.app.Activity;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class loginpage extends Activity {
+
+    EditText username;
+    EditText password;
+    Button loginButtom;
+}
